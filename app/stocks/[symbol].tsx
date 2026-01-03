ef3749489162840c8798c72fa93580cec52f176e
samedyhunx <samedy.hunx@gmail.com>
2026-01-03 19:03:27 +0700

    develop: stockdetailsscreen

--- app/stocks/[symbol].tsx
+++ app/stocks/[symbol].tsx
@@ -8,10 +8,9 @@ import {
   TECHNICAL_ANALYSIS_WIDGET_CONFIG,
 } from "@/lib/constants";
 import { scriptUrlHelper } from "@/lib/utils";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import { useMemo } from "react";
 import { ScrollView, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function StockDetailsScreen({ params }: StockDetailsPageProps) {
   const { symbol } = useLocalSearchParams();
@@ -29,47 +28,56 @@ export default function StockDetailsScreen({ params }: StockDetailsPageProps) {
   );
 
   return (
-    <SafeAreaView className="flex-1 bg-gray-50 dark:bg-gray-950">
-      <ScrollView>
-        <View className="flex-1 p-2 gap-8">
-          <TradingViewWidget
-            scriptUrl={scriptUrlHelper("symbol-info")}
-            config={symbolInfoConfig(symbol as string)}
-            height={170}
-            className="w-full rounded-2xl overflow-hidden"
-          />
-          <TradingViewWidget
-            scriptUrl={scriptUrlHelper("symbol-profile")}
-            config={companyProfileConfig(symbol as string)}
-            className="w-full rounded-2xl overflow-hidden"
-            height={440}
-          />
-          <TradingViewWidget
-            scriptUrl={scriptUrlHelper("technical-analysis")}
-            config={technicalAnalysisConfig(symbol as string)}
-            className="w-full rounded-2xl overflow-hidden"
-            height={400}
-          />
-          <TradingViewWidget
-            scriptUrl={scriptUrlHelper("financials")}
-            config={companyFinancialsConfig(symbol as string)}
-            className="w-full rounded-2xl overflow-hidden"
-            height={464}
-          />
-          <TradingViewWidget
-            scriptUrl={scriptUrlHelper("advanced-chart")}
-            config={candleChartConfig(symbol as string)}
-            className="w-full rounded-2xl overflow-hidden"
-            height={600}
-          />
-          <TradingViewWidget
-            scriptUrl={scriptUrlHelper("advanced-chart")}
-            config={baselineConfig(symbol as string)}
-            className="w-full rounded-2xl overflow-hidden"
-            height={600}
-          />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerTitle: symbol as string,
+          headerBackTitle: "Stocks",
+        }}
+      />
+      <View className="flex-1 pt-4 bg-gray-50 dark:bg-gray-950">
+        <ScrollView>
+          <View className="flex-1 p-1 gap-4">
+            <TradingViewWidget
+              scriptUrl={scriptUrlHelper("symbol-info")}
+              config={symbolInfoConfig(symbol as string)}
+              height={280}
+              className="w-full rounded-2xl overflow-hidden"
+            />
+            <TradingViewWidget
+              scriptUrl={scriptUrlHelper("symbol-profile")}
+              config={companyProfileConfig(symbol as string)}
+              className="w-full rounded-2xl overflow-hidden"
+              height={440}
+            />
+            <TradingViewWidget
+              scriptUrl={scriptUrlHelper("technical-analysis")}
+              config={technicalAnalysisConfig(symbol as string)}
+              className="w-full rounded-2xl overflow-hidden"
+              height={400}
+            />
+            <TradingViewWidget
+              scriptUrl={scriptUrlHelper("financials")}
+              config={companyFinancialsConfig(symbol as string)}
+              className="w-full rounded-2xl overflow-hidden"
+              height={464}
+            />
+            <TradingViewWidget
+              scriptUrl={scriptUrlHelper("advanced-chart")}
+              config={candleChartConfig(symbol as string)}
+              className="w-full rounded-2xl overflow-hidden"
+              height={600}
+            />
+            <TradingViewWidget
+              scriptUrl={scriptUrlHelper("advanced-chart")}
+              config={baselineConfig(symbol as string)}
+              className="w-full rounded-2xl overflow-hidden"
+              height={600}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </>
   );
 }
